1971b80b37802ef42b107f2753114faaef87d3f2
shivashedge <116933855+shivashedge@users.noreply.github.com>
2022-12-13 17:00:46 +0530

    upadted navBar After Seller Login

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  menuType: string = 'default';
+  constructor(private route: Router) {}
 
   ngOnInit(): void {
+    this.route.events.subscribe((val: any) => {
+      if (val.url) {
+        if (localStorage.getItem('seller') && val.url.includes('seller')) {
+          this.menuType = 'seller';
+        } else {
+          this.menuType = 'default';
+        }
+      }
+    });
   }
-
 }
