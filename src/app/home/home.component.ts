11fcb4f710a55bbf6aab492e7268427f1d0efd7e
shivashedge <116933855+shivashedge@users.noreply.github.com>
2022-12-20 19:03:37 +0530

    trendyProducts Added (Dynamic)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,16 +5,19 @@ import { ProductService } from '../services/product.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  popularProducts: undefined|product[]
-  constructor(private product:ProductService) { }
+  popularProducts: undefined | product[];
+  trendyProducts: undefined | product[];
+  constructor(private product: ProductService) {}
 
   ngOnInit(): void {
-    this.product.popularProducts().subscribe((data)=>{
+    this.product.popularProducts().subscribe((data) => {
       this.popularProducts = data
-    })
+    });
+    this.product.trendyProducts().subscribe((data) => {
+      this.trendyProducts = data
+    });
   }
-
 }
